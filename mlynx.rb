6041ebca36e7fab4678d864aac68118534edd4c1
Manan <mbhasin@thesummitgrp.com>
2025-07-18 03:00:47 +0530

    Added Homebrew formula for mlynx

---
+++ mlynx.rb
@@ -0,0 +1,11 @@
+class Mlynx < Formula
+  desc "MANAN LYNX – Enhanced CLI Search Tool for macOS/Linux"
+  homepage "https://github.com/mbhasin-stgi/homebrew-mlynx"
+  url "https://github.com/mbhasin-stgi/homebrew-mlynx/releases/download/v1.0.0/mlynx.tar.gz"
+  sha256 "39729a80bda15b9906d957e381242a249ba2c6ace433cce498c9c4644533f5af"
+  version "1.0.0"
+
+  def install
+    bin.install "mlynx"
+  end
+end
